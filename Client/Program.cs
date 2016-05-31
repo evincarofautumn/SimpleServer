eb86ac1f248c4dbf2ea78c0f7833d72810b84dd6
Jon Purdy <evincarofautumn@gmail.com>
2016-05-31 13:48:51 -0700

    Basic server and client programs.

---
+++ Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Client
+{
+	class ClientMain
+	{
+
+		private TcpClient client;
+
+		public static void Main (string[] args)
+		{
+			var client = new ClientMain ();
+			client.Run ().Wait ();
+		}
+
+		public ClientMain ()
+		{
+			client = new TcpClient ();
+		}
+
+		public Task Run ()
+		{
+#if DEBUG
+			Console.WriteLine ("[client] trying to connect to localhost:{0}", Server.ServerMain.Port);
+#endif
+			int retries = 3;
+			int delay = 10;
+			while (true) {
+				try {
+					client.Connect ("localhost", Server.ServerMain.Port);
+					break;
+				} catch (SocketException) {
+					Console.Error.WriteLine ("[client] ERROR connecting to server, retrying");
+					Thread.Sleep (delay);
+					delay *= 10;
+					if (retries == 0) {
+						Console.Error.WriteLine ("[client] ERROR connecting to server, no retries left");
+						System.Environment.Exit (1);
+					}
+				}
+			}
+#if DEBUG
+			Console.WriteLine ("[client] connected");
+#endif
+//			client.ReceiveTimeout = 5000;
+//			client.SendTimeout = 5000;
+			return Task.Run (() => {
+				var serverStream = client.GetStream ();
+				var request = new byte [Server.ServerMain.RequestLength];
+				var response = new byte [Server.ServerMain.ResponseLength];
+				for (int i = 0; i < Server.ServerMain.Requests; ++i) {
+#if DEBUG
+					Console.WriteLine ("[client] sending request #{0}", i);
+#endif
+					// Send request.
+					serverStream.Write (request, 0, Server.ServerMain.RequestLength);
+#if DEBUG
+					Console.WriteLine ("[client] sent request #{0}, waiting for response #{0}", i);
+#endif
+					// Wait for response.
+					serverStream.Read (response, 0, Server.ServerMain.ResponseLength);
+#if DEBUG
+					Console.WriteLine ("[client] recieved response #{0}", i);
+#endif
+				}
+#if DEBUG
+				Console.WriteLine ("[client] exchanged {0} request/response pairs, sending terminate request", Server.ServerMain.Requests);
+#endif
+				request = new byte [] { Server.ServerMain.EOT };
+				serverStream.Write (request, 0, 1);
+				serverStream.Flush ();
+#if DEBUG
+				Console.WriteLine ("[client] sent terminate request");
+#endif
+				client.Close ();
+			});
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Server
+{
+	public class ServerMain
+	{
+		public const int RequestLength = 512;
+		public const int ResponseLength = 512;
+		public const int Port = 12421;
+
+		private int workers;
+
+		public const int Requests = 10000;
+		public const byte EOT = (byte)'\x04';
+
+		private List<Task> workerTasks;
+		private Stopwatch stopwatch;
+
+		public static void Main (string[] args)
+		{
+			var workers = Int32.Parse (args [0]);
+			var server = new ServerMain (workers);
+#if DEBUG
+			Console.WriteLine ("[server] starting");
+#endif
+			server.Run ().Wait ();
+			var served = Requests * workers;
+			var elapsed = server.stopwatch.ElapsedMilliseconds;
+			Console.WriteLine (
+				"[server] workers: {0} elapsed: {1}s served: {2}req mean: {3:F2}req/s",
+				workers,
+				elapsed / 1000.0,
+				served,
+				1000.0 * served / elapsed);
+		}
+
+		public ServerMain (int workers)
+		{
+			this.workers = workers;
+			workerTasks = new List<Task> ();
+			stopwatch = new Stopwatch ();
+		}
+
+		private Task Run ()
+		{
+			TcpListener listener = null;
+			IPAddress address = Dns.GetHostEntry ("localhost").AddressList [0];
+			try
+			{
+				listener = new TcpListener (address, Port);
+				listener.Start ();
+			}
+			catch (Exception e)
+			{
+				Console.Error.WriteLine ("[server] ERROR creating TCP listener: {0}", e);
+				System.Environment.Exit (1);
+			}
+
+			return Task.Run (() => {
+				while (workerTasks.Count < workers) {
+#if DEBUG
+					Console.WriteLine ("[server] waiting for client");
+#endif
+					Socket socket = listener.AcceptSocket ();
+					var id = workerTasks.Count;
+#if DEBUG
+					Console.WriteLine ("[server] starting worker #{0}", id);
+#endif
+					workerTasks.Add (Task.Run (() => {
+						Worker (socket, id);
+					}));
+				}
+#if DEBUG
+				Console.WriteLine ("[server] waiting for all clients");
+#endif
+				stopwatch.Start ();
+				Task.WaitAll (workerTasks.ToArray ());
+				stopwatch.Stop ();
+			});
+		}
+
+		private void Worker (Socket socket, int id)
+		{
+			var buffer = new byte [RequestLength];
+			while (true) {
+#if DEBUG
+				Console.WriteLine ("[server] worker #{0} waiting for request", id);
+#endif
+				int remaining = RequestLength;
+				while (remaining > 0) {
+					int received = socket.Receive (buffer, RequestLength - remaining, remaining, SocketFlags.None);
+					// A request starting with EOT terminates the worker.
+					if (received >= 1 && buffer [0] == EOT)
+						goto end;
+					remaining -= received;
+				}
+#if DEBUG
+				Console.WriteLine ("[server] worker #{0} received request, sending response", id);
+#endif
+				// When we’ve received RequestLength bytes, we send ResponseLength bytes.
+				var reply = new byte [ResponseLength];
+				socket.Send (reply);
+#if DEBUG
+				Console.WriteLine ("[server] worker #{0} sent response", id);
+#endif
+			}
+			end:
+#if DEBUG
+			Console.WriteLine ("[server] worker #{0} received terminate request, shutting down worker #{0}", id);
+#endif
+			socket.Close ();
+		}
+	}
+}
